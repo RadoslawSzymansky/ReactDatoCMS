01c02a1073f5487b45be1b2045b8b353aae8b2a3
Radoslaw Szymanski <r.szymanski@e-intermedia.pl>
2021-05-12 22:27:27 +0200

    add dependecies

---
+++ commitlint.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    /*
+    * Resolve and load @commitlint/config-conventional from node_modules.
+    * Referenced packages must be installed
+    */
+      extends: ['@commitlint/config-conventional'],
+      /*
+       * Any rules defined here will override rules from @commitlint/config-conventional
+       */
+      rules: {
+          'type-enum': [
+              2,
+              'always',
+              ['build', 'chore', 'ci', 'docs', 'improvement', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'],
+          ],
+          "husky": {
+            "hooks": {
+              "pre-commit": "lint-staged",
+              "commit-msg": "commitlint -E HUSKY_GIT_PARAMS"
+            }
+          }
+      },
+      
+  };
