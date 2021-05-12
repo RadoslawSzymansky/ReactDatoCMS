fcd74428f563b34f6fe7a988421769ab2f8d18ca
Radoslaw Szymanski <r.szymanski@e-intermedia.pl>
2021-05-12 23:19:35 +0200

    prepare default laypout

--- src/App.js
+++ src/App.js
@@ -1,58 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Rade
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
-}
+import Header from "./Header";
+import Home from "./Home";
+import About from "./About";
+import Recipe from "./Recipe";
+import Counter from "./features/counter/Counter";
+
+const App = () => (
+  <Router>
+    <Header />
+    <main>
+      {/* <Route exact path="/" component={Home} />
+      <Route path="/recipes/:slug" component={Recipe} />
+      <Route path="/about" component={About} /> */}
+    </main>
+    <Counter />
+  </Router>
+);
 
 export default App;
